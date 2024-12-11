51fd16d48bac45e0646af5a11baf07aa2c90e78e
Ahmed Chehaibi <chehaib.ahmed@gmail.com>
2024-12-11 13:48:43 +0100

    Create api_keys.py

---
+++ api_keys.py
@@ -0,0 +1,8 @@
+# To change accordingly
+weather_key  = "OpenWeather_API_KEY_GOES_HERE"  # https://openweathermap.org/api
+weather_city = "OpenWeather_CITY_GOES_HERE"     # format "City,COUNTRYCODE" eg : "Gent,BE"
+open_ai_key  = "OpenAI_API_KEY_GOES_HERE"       # https://openai.com/index/openai-api/
+picovice_key = "PICO_API_KEY_GOES_HERE"         # https://picovoice.ai/docs/api/picovoice-python/
+colorful_id  = "TUYA_DEVICE_ID_GOES_HERE"       # Get it after running tinytuya wizard
+colorful_ip  = "TUYA_DEVICE_IP_GOES_HERE"       # Get it after running tinytuya wizard
+colorful_key = "TUYA_DEVICE_KEY_GOES_HERE"      # Get it after running tinytuya wizard
