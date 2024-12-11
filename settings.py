62ed5dffcfc268709f897abf8b1935dbef22bfc5
Ahmed Chehaibi <chehaib.ahmed@gmail.com>
2024-12-11 13:47:31 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python3
-
-# To change accordingly
-weather_key  = "OpenWeather_API_KEY_GOES_HERE"  # https://openweathermap.org/api
-weather_city = "OpenWeather_CITY_GOES_HERE"     # format "City,COUNTRYCODE" eg : "Gent,BE"
-open_ai_key  = "OpenAI_API_KEY_GOES_HERE"       # https://openai.com/index/openai-api/
-picovice_key = "PICO_API_KEY_GOES_HERE"         # https://picovoice.ai/docs/api/picovoice-python/
-colorful_id  = "TUYA_DEVICE_ID_GOES_HERE"       # Get it after running tinytuya wizard
-colorful_ip  = "TUYA_DEVICE_IP_GOES_HERE"       # Get it after running tinytuya wizard
-colorful_key = "TUYA_DEVICE_KEY_GOES_HERE"      # Get it after running tinytuya wizard
-
-
-
+from api_keys import *
 
 # Do not change unless you know what you are doing...
 # Colorful-X7
