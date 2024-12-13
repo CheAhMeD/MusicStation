d7ed8f14f54dea6b2320e3a2a8331a3a372d3a6b
Ahmed Chehaibi <chehaib.ahmed@gmail.com>
2024-12-13 12:51:04 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,16 @@ MUSIC_STATION_API_SCRIPT=/home/volumio/MusicStation/api_keys.py
 MUSIC_STATION_USER_DIR=/home/volumio/MusicStation
 MUSIC_STATION_GPIO_PATH=/sys/class/gpio
 
+#Coloring 
+RED="31"
+GREEN="32"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+BOLDGREEN="\e[1;${GREEN}m"
+BOLDBLUE="\e[1;34m"
+ITALICRED="\e[3;${RED}m"
+ENDCOLOR="\e[0m"
+
 X7_GPIO=19
 ON="1"
 OFF="0"
@@ -42,7 +52,7 @@ sudo apt-get install v4l-utils
 sudo apt-get install bluetooth bluez
 
 # Install Python packages
-echo "Installing Python packages..."
+echo -e "${BOLDGREEN}Installing Python packages...${ENDCOLOR}"
 sudo apt-get -y install python3-pip
 sudo apt-get -y install python3-pygame
 
@@ -66,24 +76,24 @@ sudo pip3 install tinytuya
 sudo pip3 install pycryptodome
 
 # Install fonts
-echo "Installing fonts..."
+echo -e "${BOLDGREEN}Installing fonts...${ENDCOLOR}"
 sudo cp $MUSIC_STATION_USER_DIR/fonts/*.ttf /usr/share/fonts
 
 # Make volumio owner
-echo "Setting ownership..."
+echo -e "${BOLDGREEN}Setting ownership...${ENDCOLOR}"
 sudo chown volumio:volumio $MUSIC_STATION_USER_DIR
 sudo chown volumio:volumio $MUSIC_STATION_API_SCRIPT
 
 # Make jarvis script executable
-echo "Making $MUSIC_STATION_RUN_SCRIPT executable..."
+echo -e "${BOLDGREEN}Making $MUSIC_STATION_RUN_SCRIPT executable...${ENDCOLOR}"
 sudo chmod +x $MUSIC_STATION_RUN_SCRIPT
 
-read -p "Do you want to setup a Colorful-X7 device? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+read -p "${BOLDBLUE}Do you want to setup a Colorful-X7 device? (Y/N): ${ENDCOLOR}" confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 echo "Starting TinyTuya Setup Wizard..."
-echo "  Before continuing make sure the steps 1 & 3 described in "
-echo "  https://github.com/jasonacox/tinytuya/tree/master?tab=readme-ov-file#setup-wizard---getting-local-keys"
-echo "  are followed..."
-read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+echo "${YELLOW} \t Before continuing make sure the steps 1 & 3 described in "
+echo " \t https://github.com/jasonacox/tinytuya/tree/master?tab=readme-ov-file#setup-wizard---getting-local-keys"
+echo " \t are followed...${ENDCOLOR}"
+read -p "${BOLDBLUE}Continue? (Y/N): ${ENDCOLOR}" confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 # Turn On the Device
 exportPin $X7_GPIO
 setOutput $X7_GPIO
@@ -94,8 +104,8 @@ sudo python3 -m tinytuya wizard
 setGpioState $X7_GPIO $OFF
 unexportPin $X7_GPIO
 
-echo "Don't forget to update $MUSIC_STATION_API_SCRIPT with the API Keys"
-echo "Finished..."
+echo "${ITALICRED}Don't forget to update $MUSIC_STATION_API_SCRIPT with the API Keys${ENDCOLOR}"
+echo "${BOLDGREEN}Finished...${ENDCOLOR}"
 
 # TODO: finish this later
 # TODO: Make jarvis script autostart
